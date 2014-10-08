097cae3923e66054cd55f5c2ad9eae6599b32d22
Jake <jmh@jakehoggans.co.uk>
2014-10-09 00:10:35 +0100

    Changed plugin name and fixed up demo

--- src/jqSlide.js
+++ src/jqSlide.js
@@ -4,7 +4,7 @@
 // Originally authored 08/2009 by Jake Hoggans (http://www.jakehoggans.co.uk)
 // Maintained from 08/2014 (https://github.com/jhoag/jqSlide)
 // ==========================================================================
-(function($){    $.fn.shufflePuzzle = function( options ){        var imageWidth, imageHeight, numPieces;
+(function($){    $.fn.jqSlide = function( options ){        var imageWidth, imageHeight, numPieces;
         var pieceWidth, pieceHeight;
         var defaults = {
             animate : true,
